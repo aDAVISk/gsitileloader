e7172427b4d967e84a2ce2a67679447090843226
aDAVISk <akito.davis@gmail.com>
2024-01-18 14:49:40 +0900

    Developed the search functions
    #5

--- src/gsitileloader/gsitileloader.py
+++ src/gsitileloader/gsitileloader.py
@@ -1,8 +1,28 @@
+import json
+import os
 from xyztilefile import *
 
 xyz = calc_xyz_from_lonlat
 
 class GSITileLoader:
+    with open(os.path.dirname(__file__)+"\\tileinfo.json", "r") as ifile:
+        tilelist = json.load(ifile)
+
+    @classmethod
+    def search(cls,key):
+        for vv in cls.tilelist:
+            if vv["tilename"] == key or vv["url"] == key:
+                return vv.copy()
+        return None
+
+    @classmethod
+    def search_about(cls,key):
+        res = []
+        for vv in cls.tilelist:
+            if key in vv["tilename"]:
+                res.append(vv.copy())
+        return res
+
     def __init__(self):
         self.loader = XYZTileFile("https://cyberjapandata.gsi.go.jp/xyz/dem5a/{z}/{x}/{y}.txt")
 
--- tests/test_load.py
+++ tests/test_load.py
@@ -1,7 +1,13 @@
 from gsitileloader import *
+import os
 import pprint
 pp = pprint.PrettyPrinter(indent=3)
 
-
 tile = GSITileLoader()
 pp.pprint(tile)
+
+pp.pprint(GSITileLoader.tilelist)
+print("---")
+pp.pprint(GSITileLoader.search_about("標高"))
+print("---")
+pp.pprint(GSITileLoader.search('https://cyberjapandata.gsi.go.jp/xyz/lakedepth/{z}/{x}/{y}.png'))
